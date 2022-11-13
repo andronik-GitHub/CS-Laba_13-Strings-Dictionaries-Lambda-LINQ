baf72e882a1f004a1211a820935ecc2bd7425c5d
kazmi <costellationoblivion@gmail.com>
2022-11-13 21:27:00 +0200

    Добавьте файлы проекта.

--- Ex6/Ex6.cs
+++ Ex6/Ex6.cs
@@ -5,45 +5,41 @@ class Ex6
 {
     static void Main()
     {
-        string? sentence = Console.ReadLine();
-        Console.WriteLine();
-
+        string? sentence = Console.ReadLine(); // речення
 
         if (sentence != null)
         {
+            // речення розбивається на слова
             string[] temp = sentence.Split('.', ',', ':', ';', '=', '(', ')', '&', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ');
-            List<string> words = new();
-            List<string> characters = new();
+            List<string> words = new(); // для слів, тому що пробіл і тд. може бути словом " = "
+            List<string> characters = new(); // для символів між словами
 
 
-            foreach (string item in temp)
+            foreach (string item in temp) // записуються слова
                 if (item.Length > 0) words.Add(item);
 
-            for (int i = 0; i < words.Count - 1; i++)
-            {
-                Console.WriteLine(sentence.IndexOf(words[i]) + words[i].Length);
-                Console.WriteLine(sentence.IndexOf(words[i + 1]));
-                Console.WriteLine();
-
-                try
-                {
-                    characters.Add
+            for (int i = 0; i < words.Count - 1; i++) // записуються символи віж словами
+                characters.Add
+                    (
+                        sentence.Substring // вирізається строка
                         (
-                            sentence.Substring
-                            (
-                                sentence.IndexOf(words[i]) + words[i].Length,
-                                sentence.IndexOf(words[i + 1])
-                            )
-                        );
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message + "\n");
-                }
-            }
+                            sentence.IndexOf(words[i]) + words[i].Length, // між першим входженням слова
+                            sentence.IndexOf(words[i + 1]) - (sentence.IndexOf(words[i]) + words[i].Length) // до початку наступного слова
+                        )
+                    );
+            
+            string newSentence = ""; // зареверснене речення
+            int index = 0; // для проходження по characters
+            for (int i = words.Count - 1; i >= 0 ; i--) // з останнього слова до першого
+            {
+                newSentence += words[i]; // додається слово
 
-            foreach (string item in characters)
-                Console.WriteLine(item);
+                if (index < characters.Count)
+                    newSentence += characters[index++]; // додається символ між словами
+            }
+            newSentence += sentence.Substring(newSentence.Length); // додаються символи після останнього слова
+
+            Console.WriteLine(newSentence); // виводиться зареверснене речення
         }
 
 
