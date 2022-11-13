4d73a4ef4efbcb4d58d6e1cff0172f8960fedb43
kazmi <costellationoblivion@gmail.com>
2022-11-13 21:27:00 +0200

    Добавьте файлы проекта.

---
+++ Ex8/Ex8.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Ex8
+{
+    static void Main()
+    {
+        string? sentence = Console.ReadLine(); // строка
+
+        if (sentence != null)
+        {
+            string[] words = sentence.Split(' ', ',', '.', '?', '!'); // розбивається на слова
+
+            List<string> palindromes = new(); // для паліндромів
+
+            foreach (string word in words) // проходиться по словам
+            {
+                int index = 0;
+                for (int i = 0; i < word.Length / 2; i++)
+                    if (word[i] == word[word.Length - i - 1]) // якщо і-ий і і-ий з кінця однакові
+                        index++;
+
+                if (index == word.Length / 2 && word.Length > 0) // якщо index == половині слова
+                {
+                    for (int j = 0; j < palindromes.Count; j++) // чи записаний вже такий паліндром
+                        if (word == palindromes[j])
+                        {
+                            index = -1;
+                            break;
+                        }
+
+                    if (index != -1) // чи записаний вже такий паліндром
+                        palindromes.Add(word); // слово додається до паліндромів}
+                }
+            }
+
+            palindromes.Sort(); // сортирується
+            Console.WriteLine(string.Join(", ", palindromes)); // виводяться всі паліндроми
+        }
+
+        
+        Console.ReadKey();
+    }
+}
