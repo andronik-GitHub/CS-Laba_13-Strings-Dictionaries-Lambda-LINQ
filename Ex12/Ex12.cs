a681381aeba26fd6ba1b37252d2b3813537acd41
kazmi <costellationoblivion@gmail.com>
2022-11-13 21:27:00 +0200

    Добавьте файлы проекта.

---
+++ Ex12/Ex12.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Ex12
+{
+    static void Main()
+    {
+        Dictionary<string, string> persons = new(); // словарь
+
+        while (true)
+        {
+            string? command = Console.ReadLine(); // команда
+
+            if (command?.ToLower() == "end") break; // завершення зчитування
+            else if (command != null)
+            {
+                string[] info = command.Split(' '); // команда розбивається
+
+                if (info[0].ToLower() == "a") // якщо А - додавання, зміна
+                {
+                    if (persons.ContainsKey(info[1])) persons[info[1]] = info[2]; // якщо є персона з таким ключом то змінюється значення
+                    else persons.Add(info[1], info[2]); // якщо немає такої персони то створюється
+                }
+                else if (info[0].ToLower() == "s") // якщо S - виведення
+                {
+                    if (persons.ContainsKey(info[1])) Console.WriteLine(info[1] + " -> " + persons[info[1]]); // якщо є така персона то виводиться
+                    else Console.WriteLine($"Contact {info[1]} does not exist."); // якщо немає персони то повідомлення про це
+                }
+            }
+        }
+
+
+        Console.ReadKey();
+    }
+}
